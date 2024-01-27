1caf1a125169c9416484aa6ac36f55d42a8933f8
ToothlessBrush <73563930+ToothlessBrush@users.noreply.github.com>
2024-01-26 21:43:00 -0600

    chest collider and camera offset

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    
+    [SerializeField] private Vector3 offset = new Vector3(0f, 2f, -10f);
     private float smoothTime = .25f;
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] private Transform target;
-
     
     // Update is called once per frame
     void Update()
