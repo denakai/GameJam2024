1c99ec0917e950aa4187a12ed529608df4b2106b
ToothlessBrush <73563930+ToothlessBrush@users.noreply.github.com>
2024-01-26 18:35:53 -0600

    added basic player movement

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float moveSpeed = 5f;  // Speed of horizontal movement
+    public float jumpForce = 10f; // Force applied when jumping
+    public Rigidbody2D Rigiedbody2D; // Reference to the Rigidbody2D component  
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,30 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        handleSideMovement();
+        handleJumpMovement();
+    }
+
+    void handleSideMovement() {
+        //move right
+        if (Input.GetKey(KeyCode.D)) {
+            Debug.Log("D pressed");
+            float newPositionX = transform.position.x + (moveSpeed * Time.deltaTime);
+            transform.position = new Vector2(newPositionX, transform.position.y);
+        }
+
+        //move left
+        if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("A pressed");
+            float newPositionX = transform.position.x - (moveSpeed * Time.deltaTime);
+            transform.position = new Vector2(newPositionX, transform.position.y);
+        }
+    }
+
+    void handleJumpMovement() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Space pressed");
+            Rigiedbody2D.velocity = new Vector2(Rigiedbody2D.velocity.x, jumpForce);
+        }
     }
 }
