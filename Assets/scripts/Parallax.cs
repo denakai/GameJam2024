e5c2f14415caa7b66a9dcf5cc88e64825dda5681
Brendan <Chermackbrendan@gmail.com>
2024-01-26 23:34:04 -0600

    semi fixed parallax

--- Assets/scripts/Parallax.cs
+++ Assets/scripts/Parallax.cs
@@ -11,9 +11,14 @@ public class FitToCamera : MonoBehaviour
         length = GetComponent<SpriteRenderer>().bounds.size.x;
 
     }
-    void Update(){
+    void Update()
+    {
+        float temp = (cam.transform.position.x * 1 - parallaxEffect);
         float dist = (cam.transform.position.x * parallaxEffect);
-        
+
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (temp < startpos - length) { startpos -= length; }
+        else if (temp > startpos + length) { startpos += length; }
+
     }
 }
