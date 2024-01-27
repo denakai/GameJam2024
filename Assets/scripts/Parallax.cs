3cff141a005d9524ac6ac6a51f02431eee784d0c
Brendan <Chermackbrendan@gmail.com>
2024-01-26 23:19:57 -0600

    parallax

---
+++ Assets/scripts/Parallax.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FitToCamera : MonoBehaviour
+{
+    private float length, startpos;
+    [SerializeField] public GameObject cam;
+    public float parallaxEffect;
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+    void Update(){
+        float dist = (cam.transform.position.x * parallaxEffect);
+        
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
