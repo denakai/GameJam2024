02e5fe862b4a41c18421c9320f8d494cee46dcf3
ToothlessBrush <73563930+ToothlessBrush@users.noreply.github.com>
2024-01-27 01:31:52 -0600

    added start menu

--- Assets/testCamera.cs
+++ Assets/testCamera.cs
@@ -1,22 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class testCamera : MonoBehaviour
 {
     public FollowCamera followCamera;
+    public Button button;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if ((Input.GetKeyDown("c"))) {
-            followCamera.switchTarget();
-        }
+    public void playGame() {
+        followCamera.switchTarget();
+        button.interactable = false;
     }
 }
