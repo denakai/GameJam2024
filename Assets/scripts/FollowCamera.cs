b59cabe5ab555d79ca793b671066e6a77ac4e291
Brendan <Chermackbrendan@gmail.com>
2024-01-27 00:02:06 -0600

    fixed parallax

--- Assets/scripts/FollowCamera.cs
+++ Assets/scripts/FollowCamera.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    
-    
+
+
     [SerializeField] private Vector3 offset = new Vector3(0f, 2f, -10f);
     private float smoothTime = .25f;
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] private Transform target;
-    
+
     // Update is called once per frame
     void Update()
     {
--- Assets/scripts/Parallax.cs
+++ Assets/scripts/Parallax.cs
@@ -13,12 +13,20 @@ public class FitToCamera : MonoBehaviour
     }
     void Update()
     {
-        float temp = (cam.transform.position.x * 1 - parallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
-        if (temp < startpos - length) { startpos -= length; }
-        else if (temp > startpos + length) { startpos += length; }
+        transform.position = new Vector3(startpos + dist, 1, transform.position.z);
+
+
+        if (temp > startpos + length)
+        {
+            startpos += length * 2;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length * 2;
+        }
 
     }
 }
